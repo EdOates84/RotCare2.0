875666af5485c4af2a65f4b77671d189b174bcb5
EdOates84 <niteshmaahwer1@gmail.com>
2020-01-20 19:16:27 +0530

    first Commit

--- app/src/main/java/com/example/android/rotcare1/LoginActivity.java
+++ app/src/main/java/com/example/android/rotcare1/LoginActivity.java
@@ -3,6 +3,8 @@ package com.example.android.rotcare1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -34,6 +36,21 @@ public class LoginActivity extends AppCompatActivity {
         Create_user = findViewById(R.id.create_user);
         forgot_pass = findViewById(R.id.forgotpass);
         fAuth = FirebaseAuth.getInstance();
+
+
+        ArrayAdapter<CharSequence> usertype_adapter = ArrayAdapter.createFromResource(this,R.array.User_type,android.R.layout.simple_spinner_item);
+        usertype_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        user_type.setAdapter(usertype_adapter);
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String Mail = mail.getText().toString().trim();
+                String Password = password.getText().toString().trim();
+                String Usertype = user_type.getSelectedItem().toString();
+            }
+        });
 
 
 
