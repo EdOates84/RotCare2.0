f0d31333ec45f49602984ef66b2cf8012f568b9b
Nitesh84 <niteshmahawer1@gmail.com>
2020-02-08 18:29:17 +0530

    Request Status

--- app/src/main/java/com/example/android/rotcare1/Req_History_Info_Activity.java
+++ app/src/main/java/com/example/android/rotcare1/Req_History_Info_Activity.java
@@ -1,14 +1,86 @@
 package com.example.android.rotcare1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Req_History_Info_Activity extends AppCompatActivity {
 
+    TextView name,token,subject,alloted,mobile,status,discription;
+    String sname,stoken,ssubject,salloted,smobile,sstatus,uiid;
+    ImageView imageView;
+    DatabaseReference mDatabase;
+    FirebaseAuth fAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_req__history__info_);
+        name = findViewById(R.id.name);
+        token = findViewById(R.id.tok);
+        subject = findViewById(R.id.sub);
+        alloted = findViewById(R.id.alloted_name);
+        mobile = findViewById(R.id.mobile_no);
+        status = findViewById(R.id.status);
+        discription = findViewById(R.id.discription);
+        fAuth = FirebaseAuth.getInstance();
+        final String Current_User = fAuth.getCurrentUser().getUid();
+        Log.e("asdfgh", "onCreate: "+Current_User );
+
+        uiid = getIntent().getStringExtra("Uiid");
+        sname = getIntent().getStringExtra("name");
+        stoken = getIntent().getStringExtra("token");
+        ssubject = getIntent().getStringExtra("subject");
+        salloted = getIntent().getStringExtra("status");
+        smobile = getIntent().getStringExtra("mobile");
+        sstatus = getIntent().getStringExtra("alloted");
+        name.setText(sname);
+        subject.setText(ssubject);
+
+
+
+
+
+
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Requests");
+        mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for ( DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()){
+                    if (uiid.equals(dataSnapshot1.getValue(Request.class).getUid())){
+
+//                        name.setText(dataSnapshot1.getValue(Request.class).getName());
+                        token.setText(String.valueOf(dataSnapshot1.getValue(Request.class).getToken()));
+                        subject.setText(dataSnapshot1.getValue(Request.class).getSub());
+                        if (dataSnapshot1.getValue(Request.class).getStatus() == 0){
+                            status.setText("Pending");
+                        }
+                        if (dataSnapshot1.getValue(Request.class).getStatus() == 1){
+                            status.setText("Process");
+                        }
+                        if (dataSnapshot1.getValue(Request.class).getStatus() == 2){
+                            status.setText("Complete");
+                        }
+//                        status.setText(dataSnapshot1.getValue(Request.class).getStatus());
+                        discription.setText(dataSnapshot1.getValue(Request.class).getDis());
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
