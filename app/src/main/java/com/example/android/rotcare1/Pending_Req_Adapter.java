ad03c76edf2e6ded83a4113c86bb20f0cc042742
Nitesh84 <niteshmahawer1@gmail.com>
2020-02-09 19:50:39 +0530

    Pending request

--- app/src/main/java/com/example/android/rotcare1/Pending_Req_Adapter.java
+++ app/src/main/java/com/example/android/rotcare1/Pending_Req_Adapter.java
@@ -20,7 +20,7 @@ public class Pending_Req_Adapter extends RecyclerView.Adapter<Pending_Req_Adapte
 
     private Context context;
     private ArrayList<Request> Request_List;
-    public String DoctorName;
+    public String Name;
 
     public Pending_Req_Adapter(Pending_req_Activity c, ArrayList<Request> u) {
         context = c;
@@ -41,6 +41,40 @@ public class Pending_Req_Adapter extends RecyclerView.Adapter<Pending_Req_Adapte
         holder.name.setText(Request_List.get(position).getName());
         holder.token.setText(String.valueOf(Request_List.get(position).getToken()));
         holder.subject.setText(Request_List.get(position).getSub());
+
+        if (Request_List.get(position).getStatus() == 0) {
+            holder.Status.setText("Pending");
+        }
+        if (Request_List.get(position).getStatus() == 1) {
+            holder.Status.setText("Process");
+        }
+        if (Request_List.get(position).getStatus() == 2) {
+            holder.Status.setText("Complete");
+        }
+//        Picasso.get().load(Request_List.get(position).getDoc_profile()).into(holder.image);
+
+
+        holder.relative.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, Req_Status_Info_Activity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                Name = Request_List.get(position).getName();
+//                Log.e("DOCTORNAME", "YOYOYOYOYO" + DoctorName);
+
+//                intent.putExtra("image", Request_List.get(position));
+                intent.putExtra("name", Request_List.get(position).getName());
+                intent.putExtra("token", Request_List.get(position).getToken());
+                intent.putExtra("subject", Request_List.get(position).getSub());
+                intent.putExtra("Uiid", Request_List.get(position).getUid());
+//                intent.putExtra("status", Request_List.get(position).getStatus());
+//                intent.putExtra("alloted_to", Request_List.get(position));
+//                intent.putExtra("mobile_no", Request_List.get(position).getStatus());
+
+                context.startActivity(intent);
+            }
+        });
 //        holder.Status.setText(String.valueOf(Request_List.get(position).getStatus()));
 //        if (Request_List.get(position).getStatus() == 0){
 //            holder.Status.setText("Pending");
@@ -81,7 +115,7 @@ public class Pending_Req_Adapter extends RecyclerView.Adapter<Pending_Req_Adapte
 
     class MyViewHolder extends RecyclerView.ViewHolder {
         ImageView image;
-        TextView name,token,subject,mobile_no;
+        TextView name, token, subject, alloted_to, mobile_no, Status;
         RelativeLayout relative;
 
         public MyViewHolder(View itemView) {
@@ -91,7 +125,9 @@ public class Pending_Req_Adapter extends RecyclerView.Adapter<Pending_Req_Adapte
             name = itemView.findViewById(R.id.name);
             token = itemView.findViewById(R.id.tok);
             subject = itemView.findViewById(R.id.sub);
-            mobile_no = itemView.findViewById(R.id.mobile_no);
+            alloted_to = itemView.findViewById(R.id.alloted_name);
+            mobile_no = itemView.findViewById(R.id.alloted_mob_no);
+            Status = itemView.findViewById(R.id.status);
             relative = itemView.findViewById(R.id.relative);
         }
     }
