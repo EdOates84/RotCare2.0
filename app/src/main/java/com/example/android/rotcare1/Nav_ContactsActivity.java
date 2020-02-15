353cdce1ce4b725aa547a436cc54c377c6edc975
Nitesh84 <niteshmahawer1@gmail.com>
2020-02-15 16:45:00 +0530

    Add Contacts

--- app/src/main/java/com/example/android/rotcare1/Nav_ContactsActivity.java
+++ app/src/main/java/com/example/android/rotcare1/Nav_ContactsActivity.java
@@ -8,9 +8,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class Nav_ContactsActivity extends AppCompatActivity {
+
+    TextView n1,p1,e1,n2,p2,e2;
+    ImageView i1,i2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +24,19 @@ public class Nav_ContactsActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar7);
         setSupportActionBar(toolbar);
+
+        n1 = findViewById(R.id.aname);
+        p1 = findViewById(R.id.apost);
+        e1 = findViewById(R.id.amail);
+        n2 = findViewById(R.id.aname2);
+        p2 = findViewById(R.id.apost2);
+        e2 = findViewById(R.id.amail2);
+        i1 = findViewById(R.id.achal_pic);
+        i2 = findViewById(R.id.pic);
+
+
+        
+
+
     }
 }
